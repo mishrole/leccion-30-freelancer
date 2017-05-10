7dab8ef3869fdbdd6a00d1b5739ff1397f0fc972
Mitchell Rodríguez <mitrodle@gmail.com>
2017-05-10 11:47:25 -0500

    Nav-bar actualizado, avance header

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-/*
- * Archivo principal de JS
- */
+var lastScroll = 60;
+
+window.onscroll = function (){
+	var header = document.getElementsByTagName("header")[0];
+	var navTitle = document.getElementsByClassName("nav-title")[0];
+	var listItem = document.getElementsByClassName("menu-list-item");
+
+	var scroll = window.pageYOffset || document.documentElement.scrollTop;
+	if(scroll > lastScroll){
+		header.classList.add("header-scroll");
+		nav-title.classList.add("navTitle-scroll");
+	}else if(scroll > 750){
+		for(var i = 0; i < listItem.length; i++){
+			listItem[i].classList.add("active");
+		}
+	}else{
+		header.classList.remove("header-scroll");
+		navTitle.classList.remove("navTitle-scroll");
+		for(var i = 0; i < listItem.length; i++){
+			listItem[i].classList.add("active");
+		}
+	}
+
+};
